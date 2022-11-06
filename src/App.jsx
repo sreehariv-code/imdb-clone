0c38393f4942e185fb8631a034fb27d5602367bd
SREEHARI P V <sreehariv07@gmail.com>
2022-11-06 16:14:03 +0530

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react';
+import { Header } from './components/Header';
 import './App.css'
 
 function App() {
-  const [ count, setCount ] = useState( 0 )
+  
 
   return (
     <div className="App">
+      <Header className="header"/>
       <h1>Hello world</h1>
     </div>
   )
